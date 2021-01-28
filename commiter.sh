bd57b130704938463f50e89a9e3f318c26148498
StasQuasilin <stasvasilin@gmail.com>
2021-01-28 15:09:10 +0200

    28.01.2021 dev

--- commiter.sh
+++ commiter.sh
@@ -1,8 +1,14 @@
 #! /bin/bash
+changes=`git status -s | wc -l`
 
-git add .
-git add -u
+if [[ changes -gt 0 ]]
+then
+	git add .
+	git add -u
 
-date=`date +"%d.%m.%Y dev"`
-commit=git commit -m "${date}" 
+	msg=`date +"%d.%m.%Y dev"`
+	git commit -m "${msg}" 
+
+	git push
+fi
 
